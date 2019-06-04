8289b63f8414c71bdb267c49c068964f8e3166ac
yuchunwujixian <342338015@qq.com>
2019-06-04 22:11:30 +0800

    test

--- database/migrations/2016_11_10_034922_create_admin_permissions_table.php
+++ database/migrations/2016_11_10_034922_create_admin_permissions_table.php
@@ -19,7 +19,6 @@ class CreateAdminPermissionsTable extends Migration
             $table->string('label')->comment('权限解释名称');
             $table->string('description')->comment('描述与备注');
             $table->tinyInteger('cid')->comment('级别');
-            $table->tinyInteger('is_menu')->comment('级别');
             $table->string('icon')->comment('图标');
             $table->tinyInteger('is_menu')->default(0)->comment('是否菜单 0否 1是');
             $table->string('params')->nullable()->comment('额外参数，直接字符串拼接');
